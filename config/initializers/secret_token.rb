3a90d0518be9376c1104bbc286af2f574ba38892
Johanns Gregrian <io.johannsg+git@gmail.com>
2011-07-05 21:46:50 -0700

    Initial Commit: New Rails App
    Rails: 3.1.0-rc4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Celox::Application.config.secret_token = '4943dfd1074b5cd4a4c086014262077b0f0cd8199ee3c294d35fdbe36437bf4a8bc766794ad19129ba9b24ca57d6c353649749621879a3f6ea32505941cef38a'
