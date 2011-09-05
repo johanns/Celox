6543b89141b47847cd96b330d6c0ec47405e2875
Johanns Gregorian <io.johannsg+git@gmail.com>
2011-09-05 15:50:51 -0700

    ZeroClipboard files (not integrated yet).

--- app/assets/javascripts/form_misc.js
+++ app/assets/javascripts/form_misc.js
@@ -1,4 +1,24 @@
-function setFocus()
-{
+function setFocus() {
   $("#message_body").focus();
 }
+
+$(document).ready(function() {
+
+    $('a#copy-description').zclip({
+        path:'js/ZeroClipboard.swf',
+        copy:$('p#description').text()
+    });
+
+    // The link with ID "copy-description" will copy
+    // the text of the paragraph with ID "description"
+
+
+    $('a#copy-dynamic').zclip({
+        path:'js/ZeroClipboard.swf',
+        copy:function(){return $('input#dynamic').val();}
+    });
+
+    // The link with ID "copy-dynamic" will copy the current value
+    // of a dynamically changing input with the ID "dynamic"
+
+});
