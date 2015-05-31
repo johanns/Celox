ace2d22e788d6899c262c46eba070eb293b67bc0
Johanns Gregorian <io+git@jsg.io>
2015-05-31 16:17:40 -0700

    Application: View: Added 'footer'

---
+++ app/assets/javascripts/application.coffee
@@ -0,0 +1,8 @@
+#= require jquery
+#= require jquery.turbolinks
+#= require jquery_ujs
+#= require jquery-readyselector
+#= require uikit/uikit
+#= require sjcl
+#= require turbolinks
+#= require_tree .
--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-//= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require jquery-readyselector
-//= require uikit/uikit
-//= require sjcl
-//= require turbolinks
-//= require_tree .
