f1a975c891beda2bfc26308feed7a6863700a657
Johanns Gregorian <johanns@fingerprintplay.com>
2017-02-28 13:03:41 -0800

    Krypto: Lib: Removed obsolete code

--- lib/krypto.rb
+++ lib/krypto.rb
@@ -38,15 +38,4 @@ module Krypto
   def self.salt_shaker(length:)
     OpenSSL::Random.random_bytes(length)
   end
-
-  # Copied from Rails (edge 5.0): rails/activesupport/lib/active_support/core_ext/securerandom.rb
-  # Copyright (c) 2005-2015 David Heinemeier Hansson
-  BASE58_ALPHABET = ('0'..'9').to_a  + ('A'..'Z').to_a + ('a'..'z').to_a - ['0', 'O', 'I', 'l']
-  def self.base58(n)
-    SecureRandom.random_bytes(n).unpack("C*").map do |byte|
-      idx = byte % 64
-      idx = SecureRandom.random_number(58) if idx >= 58
-      BASE58_ALPHABET[idx]
-    end.join
-  end
 end
