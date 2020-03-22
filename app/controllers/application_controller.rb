33594133408d6656aed11c05331a3561948b1ee5
Johanns Gregorian <io@jsg.io>
2020-03-21 20:58:02 -0700

    Application: Controller: Removed obsolete code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-	
-	def render_404
-	  render file: "#{Rails.root}/public/404.html", layout: false, status: 404
-  end
-end
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base; end
