6b2519956a98817e70b067a5b7b668e66dbdf0b9
Johanns Gregorian <io@jsg.io>
2025-07-06 12:14:07 -0700

    feat(assets): add SVG assets and IconsHelper module for rendering

---
+++ app/helpers/icons_helper.rb
@@ -0,0 +1,23 @@
+module IconsHelper
+  def icon(name, options = {})
+    # Read the SVG file from the assets pipeline
+    file = File.read(Rails.root.join("app", "assets", "icons", "#{name}.svg"))
+    # Create a new Nokogiri document from the SVG file
+    doc = Nokogiri::HTML::DocumentFragment.parse(file)
+    # Get the svg element
+    svg = doc.at_css "svg"
+
+    # Add any additional classes to the svg element
+    svg["class"] = "#{svg['class']} #{options[:class]}" if options[:class].present?
+
+    # Add any additional data attributes to the svg element
+    if options[:data].present?
+      options[:data].each do |key, value|
+        svg["data-#{key}"] = value
+      end
+    end
+
+    # Return the svg element as a string
+    doc.to_html.html_safe
+  end
+end
