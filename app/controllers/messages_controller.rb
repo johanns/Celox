0e5cf71d8353fe2aa772cd3f0ed4542425b36e2e
Johanns Gregorian <io@jsg.io>
2025-07-08 21:27:40 -0700

    feat: implement challenge system for message retrieval and add fetch route

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,32 @@
 # frozen_string_literal: true
 
 class MessagesController < ApplicationController
+  include Challengeable
+
+  before_action(:set_message, only: %i[fetch show])
+
   def show
-    message = Message.active.find_by!(stub: params[:stub])
-    @message_body, @message_read_at = message.read!
+    # Check if message was already read
+    if @message.read?
+      @message_read_at = @message.read_at
+      @message_body = nil
+      return
+    end
+
+    setup_challenge_session
+
+    # Don't call read! here anymore - we'll do it in fetch action after captcha
+    @message_body = nil
+    @message_read_at = nil
+  end
+
+  def fetch
+    return render_challenge_error unless valid_challenge?
+
+    clear_challenge_session
+    message_body, message_read_at = @message.read!
+
+    render_success_response(body: message_body, read_at: message_read_at)
   end
 
   def new
@@ -14,24 +37,17 @@ class MessagesController < ApplicationController
     @message = Message.new(processed_message_params)
 
     if @message.save
-      render(json: {
-               success: true,
-               message: {
-                 stub: @message.stub,
-                 retrieval_url: read_message_url(@message.stub)
-               }
-             },
-             status: :created)
+      render_created_response
     else
-      render(json: {
-               success: false,
-               errors: @message.errors.to_hash(true)
-             },
-             status: :unprocessable_entity)
+      render_validation_errors
     end
   end
 
   private
+
+  def set_message
+    @message = Message.active.find_by!(stub: params[:stub])
+  end
 
   def message_params
     params.expect(message: %i[body expiration_duration])
@@ -45,5 +61,33 @@ class MessagesController < ApplicationController
     message_params.merge(
       expiration_duration: normalize_expiration_duration(message_params[:expiration_duration])
     )
+  end
+
+  # JSON response helpers
+  def render_success_response(body:, read_at:)
+    render(json: {
+             success: true,
+             body: body,
+             read_at: read_at
+           })
+  end
+
+  def render_created_response
+    render(json: {
+             success: true,
+             message: {
+               stub: @message.stub,
+               retrieval_url: read_message_url(@message.stub)
+             }
+           },
+           status: :created)
+  end
+
+  def render_validation_errors
+    render(json: {
+             success: false,
+             errors: @message.errors.to_hash(true)
+           },
+           status: :unprocessable_entity)
   end
 end
