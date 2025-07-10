f40877f1054f0be45e42a78bb44fe07631cf3ae9
Johanns Gregorian <io@jsg.io>
2025-07-09 17:27:32 -0700

    fix: update nonce generator for content security policy to use SecureRandom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  # allow_browser(versions: :modern)
+  allow_browser(versions: :modern)
 
   rescue_from(ActiveRecord::RecordNotFound, with: :handle_record_not_found)
 
